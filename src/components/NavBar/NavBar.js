5bdcf7b3a154a3b77cef7f179b2034dd46b8dceb
viettov <viettov@gmail.com>
2022-07-05 22:47:41 -0300

    segundo desafio

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+const NavBar = () => {
+    return (
+        <div className="navBar">
+            <header className="header">
+                <div class="logo">
+                    <a href="#">TIENDA DE ROPA</a>
+                </div>
+                <nav class="nav">
+                    <a href="#">Inicio</a>
+                    <a href="#">Mis viajes</a>
+                    <a href="#">Acceso</a>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default NavBar;
