53c4ccc0de885280f4670c18c18bda7847bd65e9
viettov <viettov@gmail.com>
2022-07-13 01:09:46 -0300

    cuarto desafio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <>
       <NavBar/>
       <ItemListContainer titulo='Título Provisional' descripcion='----'/>
+      <ItemCount stock={5} initial={0}/>
     </>
   );
 }
